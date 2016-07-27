c714dd64e87c51ec2901c070a096505c6f6024ed
yonkylin <yonkylin@qq.com>
2016-07-27 15:45:06 +0800

    Frist commit.

---
+++ src/com/coolweather/app/model/Province.java
@@ -0,0 +1,26 @@
+package com.coolweather.app.model;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+	
+}
